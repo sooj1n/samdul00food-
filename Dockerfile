02392d14ce0f53ae162cc49481f18d646ec0dc44
sooj1n <sujin19371@gmail.com>
2024-09-13 10:26:26 +0900

    0.1.1

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.11
+
+WORKDIR /code
+
+COPY src/samdul00food/main.py /code/
+
+RUN pip install --no-cache-dir --upgrade git+https://github.com/sooj1n/samdul00food-.git@0.1.1
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080", "--reload"]
---
+++ src/samdul00food/main.py
@@ -0,0 +1,20 @@
+from typing import Union
+from fastapi import FastAPI
+import pickle
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+	return {"Hello": "n23"}
+	
+@app.get("/food")
+def food(name:str):
+	#시간을 구함
+	#음식 이름과 시간을 csv로 저장 -> /code/data/food.csv
+    # DB 저장 
+    print("=================================" + name)
+
+    return {"food":name, "time":"2024-09-15 11:12:13"}
+	
+
